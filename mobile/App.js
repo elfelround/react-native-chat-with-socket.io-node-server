ec4b721f7004d1af2167481c60cc60ef6ce08876
mr=programs <livingandwasting@hotmail.com>
2019-01-11 00:55:21 +0100

    quoted out a bunch of stuff and installed socket.io-client, commiting before adding new code

--- mobile/App.js
+++ mobile/App.js
@@ -1,4 +1,4 @@
-/**
+/*remove me from first refractoring
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
@@ -9,21 +9,22 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
+/*const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+###removed on first refractoring*/
 
-type Props = {};
-export default class App extends Component<Props> {
+/* type Props = {}; */
+export default class App extends Component/*<Props>*/ {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {/*<Text style={styles.instructions}>{instructions}</Text>*/}
       </View>
     );
   }
