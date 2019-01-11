2c1d69115c7574a0c66c1c19d8407c0268027027
mr=programs <livingandwasting@hotmail.com>
2019-01-11 20:28:51 +0100

    server & app work

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,16 @@ const port = 3000;
 
 io.on("connection", socket => {
     console.log("a user connected :D");
+    socket.on("chat message"/*tag added on app.js
+    when sending the io.emit to identify*/, msg => {
+        console.log(msg);
+        io.emit/* funny that this is both used on the client
+        and server, this will send the received message to 
+        all of the clients listening to the SOCKET IO SERVER
+        (THE IP WE USED, so i guess u need new ip/port for each
+        chat room), sorta like a broacasted
+        post req */("chat message", msg);
+    });
 });
 
 server.listen(port, () => console.log("server is running on port" + port));
--- mobile/App.js
+++ mobile/App.js
@@ -23,23 +23,36 @@ export default class App extends Component/*<Props>*/ {
   constructor(props) {
     super(props);
     this.state = {
-      chatMessage: ""
+      chatMessage: "",
+      chatMessages: []
     };
   }
 
   componentDidMount() {
     this.socket = io("http://192.168.1.133:3000");
     //never put inside constructor anything async like this
+    this.socket.on("chat message", msg => {
+      this.setState({ /* array we set before, and the ... (spread operator from es6)
+        stands for the array we already have plus... */
+        chatMessages: [...this.state.chatMessages, msg] });
+        //subrayar esto como buena forma para add cue to arrays
+    })
   }
 
   submitChatMessage() {
     this.socket.emit("chat message", this.state.chatMessage)
-    //emt method big deal
+    //emit method big deal, guess its like sending a post r
     this.setState({ chatMessage: "" });
 
   }
+/* best practice in react to add a key for each element in an array,
+being lazy and setting just the key as a chatMessage but real scenario
+you sould use unique key or unique ID*/
 
   render() {
+
+    const chatMessages = this.state.chatMessages.map(chatMessage => <Text   
+      key={chatMessage}>{chatMessage}</Text>)
     return (
       <View style={styles.container}>
         <TextInput
@@ -50,6 +63,7 @@ export default class App extends Component/*<Props>*/ {
         onChangeText={chatMessage => {
           this.setState({ chatMessage });
         }} ></TextInput>
+        {chatMessages}
       </View>
     );
   }
@@ -64,3 +78,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   }
 });
+
+//todo: set the key to uniqueID
