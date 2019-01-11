646ff83f09f5e12d506209cfe14b46bb3891f5ed
mr=programs <livingandwasting@hotmail.com>
2019-01-11 19:59:04 +0100

    front emmits now, back still lacks listener

--- mobile/App.js
+++ mobile/App.js
@@ -7,7 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {TextInput, StyleSheet, Text, View} from 'react-native';
+import io from "socket.io-client";
 
 /*const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,32 +20,47 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 
 /* type Props = {}; */
 export default class App extends Component/*<Props>*/ {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMessage: ""
+    };
+  }
+
+  componentDidMount() {
+    this.socket = io("http://192.168.1.133:3000");
+    //never put inside constructor anything async like this
+  }
+
+  submitChatMessage() {
+    this.socket.emit("chat message", this.state.chatMessage)
+    //emt method big deal
+    this.setState({ chatMessage: "" });
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        {/*<Text style={styles.instructions}>{instructions}</Text>*/}
+        <TextInput
+        style={{ height: 40, borderWidth: 2}}
+        autoCorrect={false}
+        value={this.state.chatMessage}
+        onSubmitEditing={() => this.submitChatMessage()}
+        onChangeText={chatMessage => {
+          this.setState({ chatMessage });
+        }} ></TextInput>
       </View>
     );
   }
 }
+        /* es6 shortcut, just saying chatMessage
+          instead of chatMessage = chatMessage because
+          they have the same name*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
